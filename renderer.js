6e47246fe1079b34411683e530d501d8d0bea091
michi <tanakamichi7@gmail.com>
2025-04-06 23:14:24 -0700

    initial commit

---
+++ renderer.js
@@ -0,0 +1,11 @@
+document.getElementById('play').addEventListener('click', () => {
+    console.log('Toggle play/pause');
+})
+
+document.getElementById('prev').addEventListener('click', () => {
+    console.log('Previous track');
+})
+
+document.getElementById('next').addEventListener('click', () => {
+    console.log('Next track');
+})
