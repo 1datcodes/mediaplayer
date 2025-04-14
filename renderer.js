5c2bc8609ba61a6712b244d1c7f7682318439076
michi <tanakamichi7@gmail.com>
2025-04-13 22:09:12 -0700

    informations displayed correctly

--- renderer.js
+++ renderer.js
@@ -15,5 +15,18 @@ document.getElementById('next').addEventListener('click', () => {
 
 setInterval(async () => {
     const info = await getMediaInfo();
-    console.log("Now Playing: ", info);
+    // console.clear();
+    // console.log("Now Playing: ", info);
+
+    document.getElementById('title').textContent = info[0].title;
+    document.getElementById('artist').textContent = info[0].artist;
+    // document.getElementById('album').textContent = info[0].album;
+    // console.log('Album Cover: ', info[0].coverArt);
+
+    const albumArt = document.getElementById('albumArt');
+    if (info[0].coverArt) {
+        albumArt.src = info[0].coverArt;
+    } else {
+        albumArt.src = 'images/fallback.png'; // Fallback image if cover art is not available
+    }
 }, 3000)
